c63926a293d2b2d460cbe84695db5572c55a059c
cifromig <75300259+cifromig@users.noreply.github.com>
2022-05-18 09:28:01 +0700

    Домашние задание на предмет работы с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,28 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  
+  if (arr1.length === arr2.length) {
+    function isBigEnough(element, index, array) {
+      return element === arr2[index];
+    }
+    result = arr1.every(isBigEnough); 
+  }
+  else {
+    result = false
+  }
   return result; // boolean
 }
+
+// let res = compareArrays ([1,2,3], [1,2,3])
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+      resultArr = arr.filter(item => item>0);
+      resultArr = resultArr.filter(item => item%3 === 0);
+      resultArr = resultArr.map(item => item*10);
+  
   return resultArr; // array
 }
+
+advancedFilter ([-1,1,2,3])
